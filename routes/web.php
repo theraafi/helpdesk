eb5f294cd9acd5c4f43538aad6508bd51a72b53d
theraafi <raafi.gbil@outlook.com>
2025-12-16 06:21:15 +0600

    routes testing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpKernel\Event\ViewEvent;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('users/hello', function () {
+//     return View('Raafi');
+// })->name('users.hello');
